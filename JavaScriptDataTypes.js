cbdf500af74c58dfab9a9a53c64307276d3a641a
Milan Keselj <68059464+keselj@users.noreply.github.com>
2023-05-05 15:09:40 +0200

    Add files via upload

---
+++ JavaScriptDataTypes.js
@@ -0,0 +1,25 @@
+// Numbers
+let number1 = 11;
+let number2 = 2.15;
+
+// String
+
+let string1 = "Decenter";
+let string2 = "Milan got intership in Decenter :D."
+
+// Booleans
+
+let bool1 = true;
+let bool2 = false;
+
+// Object
+
+const person = {firstName: "Vitalik", lastName: "Buterin "};
+
+// Array Object:
+
+const cars = ["Zastava", "Mazda", "Volvo"];
+
+// Data Objects:
+
+const data = new Data("2022-03-25");
