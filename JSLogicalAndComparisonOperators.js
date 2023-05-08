19066c3b00d2fc4e1136f751723be180c2fa62dd
Milan Keselj <68059464+keselj@users.noreply.github.com>
2023-05-08 10:34:49 +0200

    Add files via upload

---
+++ JSLogicalAndComparisonOperators.js
@@ -0,0 +1,58 @@
+// Comparison Operators
+
+x = 11;
+
+// equal to (==) 
+
+x == 7; // false
+x == 11 // true
+x == "11" // true
+
+// equal value and equal type (===)
+
+x === 11; // true
+x === "11" // false
+
+// not equal (!=)
+
+x != 7; // true
+
+// not equal value or not equal type (!==)
+
+x !== 11; // false
+x !== "11" // true
+x !== 7 // true
+
+// greater than
+
+x > 7 // true
+
+// less than
+
+x < 7 // false
+
+// greater than or equal to
+
+x >= 7 // true
+
+// less than or equal to
+
+x <= 7 // false
+
+
+// Logical Operators
+
+x = 11;
+y = 7;
+
+// And Operator (&&)
+
+(x < 15 && y > 1) //true
+
+// Or Operator (||)
+
+(x == 7 || y == 12) // false
+
+// not Operator (!)
+
+!(x == y) // true
